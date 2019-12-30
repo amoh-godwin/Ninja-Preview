b32729a93e8dc4d1dc79d12f54b952d9fedf7414
amoh-godwin <amohgyebigodwin@gmail.com>
2019-12-30 18:56:20 +0000

    Update qmlview version

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ setup(
     name="Ninja_Preview",
     version="1.1",
     packages=find_packages(),
-    install_requires=['PyQt5 >= 5.10', 'Qmlview'],
+    install_requires=['PyQt5 >= 5.10', 'Qmlview >= 1.1'],
     entry_points={
             'gui_scripts': ['Ninja_Preview = Ninja_Preview.Ninja_preview:dummy_run'],
     },
     
     author="Amoh - Gyebi Godwin Ampofo Michael",
     author_email="amohgyebigodwin@gmail.com",
-    description="A preview your Qml files",
+    description="A preview for your Qml files",
     keywords="qmlview, qmlscene, qml, pyqt, pyqt5, pyside, pyside2",
     url="https://github.com/amoh-godwin/Ninja-Preview/",   # project home page, if any
     project_urls={

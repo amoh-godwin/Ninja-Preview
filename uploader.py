d47eec811829ff84ba4086620c2ae2ebbe8ab824
amoh-godwin <amohgyebigodwin@gmail.com>
2021-02-17 17:23:14 +0000

    Update uploader.py

--- uploader.py
+++ uploader.py
@@ -10,7 +10,7 @@ version = os.environ['GITHUB_REF'].split('/')[-1]
 print(f'version: {version}')
 
 _, os_name, token = sys.argv
-print('token length: ', len(token))
+
 osn = os_name.split('-')[0]
 cwd = os.path.realpath('.')
 
@@ -32,9 +32,7 @@ def change_iss():
 
         with open('ninja_preview_setup.iss', 'w') as iss_write:
             iss_write.write(conts)
-            print('\n\n', conts, '\n\n')
 
-        print(os.listdir('.'))
 
 # Login to GH
 with open('token.txt', 'w') as tok:
@@ -49,16 +47,15 @@ print('Authenticated')
 # Build archives
 if os_name == 'windows-latest':
     # Download Qmlview archive for os
-    os_cmd = 'gh release download --repo github.com/amoh-godwin/Qmlview --pattern "*.zip"'
+    os_cmd = 'gh release download --repo'
+    os_cmd += ' github.com/amoh-godwin/Qmlview --pattern "*.zip"'
     os.system(os_cmd)
-    print('done with download')
-    print(os.listdir(cwd))
+    print('done with download of Qmlviews')
     # extract to folder
     arch = glob.glob('qmlview*.zip')[0]
     shutil.unpack_archive(arch, extract_dir=folder_name)
-    # copy all those contents to folder_name, skipping the existing ones
+    # copy all those contents to folder_name, skip exitsting
     print('done with unpack')
-    print(os.listdir(folder_name))
     # instead of a zip make Inno setup file
     # Extract inno setup
     shutil.unpack_archive(os.path.join(cwd, 'inno.zip'))
@@ -69,41 +66,47 @@ if os_name == 'windows-latest':
     os.chdir('inno')
     inno_cmd = f'iscc "{inno_script}"'
     out = subprocess.run(inno_cmd, capture_output=True)
-    print(out)
-    print(os.listdir(dist_folder))
     print('Inno script done changing back to directory')
     # change directory back
     os.chdir('..')
 
     # rename
     try:
-        old_file = os.path.join(cwd, 'dist', f"Ninja-Preview-{version[1:]}-setup.exe")
-        filename = os.path.join(cwd, 'dist', f'Ninja_Preview_{version}_{osn}-setup.exe')
+        old_file = os.path.join(cwd, 'dist',
+         f"Ninja-Preview-{version[1:]}-setup.exe")
+
+        filename = os.path.join(cwd, 'dist',
+         f'Ninja_Preview_{version}_{osn}-setup.exe')
+
         os.replace(old_file, filename)
 
     except:
         print('failed trying something else')
         print(os.listdir(dist_folder))
-        old_file = os.path.join(cwd, 'dist', f"Ninja-Preview-{version[1:]}-setup")
-        filename = os.path.join(cwd, 'dist', f'Ninja_Preview_{version}_{osn}-setup.exe')
+        old_file = os.path.join(cwd, 'dist',
+         f"Ninja-Preview-{version[1:]}-setup")
+
+        filename = os.path.join(cwd, 'dist',
+         f'Ninja_Preview_{version}_{osn}-setup.exe')
+
         os.replace(old_file, filename)
 
 else:
-    sys.exit(0)
     # Download Qmlview archive for os
-    os_cmd = 'gh release download --repo github.com/amoh-godwin/Qmlview --pattern "*.tar.gz"'
+    os_cmd = 'gh release download --repo'
+    os_cmd += ' github.com/amoh-godwin/Qmlview --pattern "*.tar.gz"'
     os.system(os_cmd)
-    print('done with download')
-    print(os.listdir(cwd))
+    print('done with download of Qmlview')
     # extract to folder
     arch = glob.glob('qmlview*.tar.gz')[0]
     shutil.unpack_archive(arch, extract_dir=folder_name)
     print('done with unpack')
-    print(os.listdir(folder_name))
-    # copy all those contents to folder_name, skipping the existing ones
+    # copy all those contents to folder_name, skip existing
     # tar.gz file
-    old_file = shutil.make_archive('Ninja_Preview', 'gztar', root_dir=dist_folder, base_dir=folder_name)
-    filename = os.path.join(dist_folder, f'Ninja_Preview_{version}_{osn}.tar.gz')
+    old_file = shutil.make_archive('Ninja_Preview', 'gztar',
+     root_dir=dist_folder, base_dir=folder_name)
+    filename = os.path.join(dist_folder,
+     f'Ninja_Preview_{version}_{osn}.tar.gz')
     os.replace(old_file, filename)
 
 
